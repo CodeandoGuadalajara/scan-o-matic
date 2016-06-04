2e477dd4dd4e2b144c905f205da3af7663b5883f
iorch <j.martinezortega@gmail.com>
2016-06-03 23:17:40 -0500

    renombrando scripts de sql

--- data/createdb.sql
+++ data/createdb.sql
@@ -1,30 +1,27 @@
 create DATABASE scanomatic;
 use scanomatic;
-create table alimentos(
+create table alimentos (
 	id int PRIMARY KEY auto_increment,
 	Alimento varchar (64),
 	Calorias float (4), 
-	Grasas totales float (4),
-	Acidos grasos saturados float (4),
-	Acidos grasos poliinsaturados float (4),
-	Acidos grasos monoinsaturados float (4),
+	Grasas_total float (4),
+	Acidos_grasos_saturados float (4),
+	Acidos_grasos_poliinsaturados float (4),
+	Acidos_grasos_monoinsaturados float (4),
 	Colesterol float (4),
 	Sodio float (4),
 	Potasio float (4),
-	Hidratos de Carbono float (4),
-	Fibra alimentaria float (4),
-	Azúcares float (4),
+	Carbohidratos float (4),
+	Fibra float (4),
+	Azucares float (4),
 	Proteinas float (4),
 	Hierro float (4),
 	Calcio float (4),
 	Magnesio float (4),
-	Vitamina A float (4),
-	Vitamina B6 float (4),
-	Vitamina B12 float (4),
-	Vitamina C float (4),
-	Vitamina D float (4),
-	Vitamina E float (4),
-	Vitamina F float (4),
-	
-
-);
+	VitaminA float (4),
+	VitaminB6 float (4),
+	VitaminB12 float (4),
+	VitaminC float (4),
+	VitaminD float (4),
+	VitaminE float (4),
+	VitaminF float (4))
