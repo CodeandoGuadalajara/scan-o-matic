9b4b106371c9f4d611cf5c943a8d1da9a20dfb2f
iorch <j.martinezortega@gmail.com>
2016-06-04 00:01:45 -0500

    using database

--- data/insert.sql
+++ data/insert.sql
@@ -1,3 +1,5 @@
+use scanomatic;
+
 INSERT INTO alimentos VALUES (0,'banana',89,.3,.1,.1,0,0,1,358,23,2.6,12,1.1,1,0,6,1,20,0,14,0,0,0);
 
 INSERT INTO alimentos VALUES (0,'apple',52,.17,.028,.051,.007,0,1,107,13.8,2.4,10.3,.26,1,1,1,0,0,1,0,0,0,0);
